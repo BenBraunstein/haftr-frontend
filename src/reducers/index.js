3d11cca9057bb9cfdad47c9a2f0452e2cc6a2219
BenBraunstein <BenZBraunstein@gmail.com>
2020-04-08 00:46:34 -0400

    redux boilerplate

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import defaultState from "./defaultState"
+
+const rootReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default rootReducer
