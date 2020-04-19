4ba5e1055a940447bec98738af0d157f119a8d23
Ben Braunstein <BenZBraunstein@gmail.com>
2020-04-19 19:33:14 -0400

    export csv created

--- src/AlumniTable.js
+++ src/AlumniTable.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Table, Header } from "semantic-ui-react"
+import { Table, Header, Button, Icon } from "semantic-ui-react"
 import TableRow from "./TableRow"
 import { useSelector } from "react-redux"
 import NewAlumniModal from "./NewAlumniModal"
@@ -7,11 +7,33 @@ import NewAlumniModal from "./NewAlumniModal"
 function AlumniTable() {
   let state = useSelector((state) => state)
 
+  const downloadCsv = () => {
+    let csv = "Name,Email Address,Cell Phone,HILLEL,HILI,HAFTR\n"
+    csvData.forEach(function (row) {
+      csv += row.join(",")
+      csv += "\n"
+    })
+    const hiddenElement = document.createElement("a")
+    hiddenElement.href = "data:text/csv;charset=utf-8," + encodeURI(csv)
+    hiddenElement.target = "_blank"
+    hiddenElement.download = "alumni.csv"
+    hiddenElement.click()
+  }
+
   const matchingAlum = state.allAlumni.filter((alum) =>
     `${alum.firstName} ${alum.middleName} ${alum.lastName}`
       .toLowerCase()
       .includes(state.searchBarText.toLowerCase())
   )
+
+  const csvData = matchingAlum.map((alum) => [
+    `${alum.firstName} ${alum.lastName}`,
+    alum.emailAddress,
+    alum.cellPhone,
+    alum.hillel,
+    alum.hili,
+    alum.haftr,
+  ])
 
   const matchingAlumniRows = matchingAlum.map((alum) => (
     <TableRow key={`alum${alum.id}`} alumInfo={alum} />
@@ -35,6 +57,16 @@ function AlumniTable() {
         <Table.Footer fullWidth>
           <Table.Row>
             <Table.HeaderCell colSpan="6">
+              <Button
+                floated="left"
+                icon
+                labelPosition="left"
+                primary
+                size="small"
+                onClick={downloadCsv}
+              >
+                <Icon name="download" /> Export to CSV
+              </Button>{" "}
               {/* Will Show as Button with Icon */}
               <NewAlumniModal />
             </Table.HeaderCell>
