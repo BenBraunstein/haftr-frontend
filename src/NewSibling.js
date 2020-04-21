7973ee2ed181b2c1b1228cd0c2db65194ddd7117
Ben Braunstein <BenZBraunstein@gmail.com>
2020-04-21 10:41:03 -0400

    sibling is now controlled for new alum

--- src/NewSibling.js
+++ src/NewSibling.js
@@ -1,23 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 import { Form } from "semantic-ui-react"
 
 export default function NewSibling(props) {
+  const [formInfo, changeFormInfo] = useState(
+    props.siblingInfo
+      ? props.siblingInfo
+      : {
+          [`sibling${props.count}Name`]: "",
+          [`sibling${props.count}Year`]: "",
+          [`sibling${props.count}School`]: "",
+        }
+  )
+
+  const handleChange = (e) => {
+    changeFormInfo({
+      ...formInfo,
+      [e.target.name]: e.target.value,
+    })
+  }
+
   return (
     <Form.Group>
       <Form.Input
         label="Name"
         placeholder="Name"
         name={`sibling${props.count}Name`}
+        value={formInfo[`sibling${props.count}Name`]}
+        onChange={handleChange}
       />
       <Form.Input
         label="Year Finished"
         placeholder="Year Finished"
         name={`sibling${props.count}Year`}
+        value={formInfo.yearFinished}
+        onChange={handleChange}
       />
       <Form.Input
         label="School"
         placeholder="School"
         name={`sibling${props.count}School`}
+        value={formInfo.school}
+        onChange={handleChange}
       />
     </Form.Group>
   )
